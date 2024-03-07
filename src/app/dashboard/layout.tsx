240f0b83da3df4300266d12300b770eb266408df
Josh Daniel <joshxfi.dev@gmail.com>
2024-03-07 20:05:29 +0800

    feat: setup urql client

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,20 +1,37 @@
-import { DashboardNavbar } from "@/components/dashboard/navbar";
-import type { Metadata } from "next";
+"use client";
 
-export const metadata: Metadata = {
-  title: "Lentroph | Dashboard",
-  description: "Connecting organizations for a better tomorrow.",
-};
+import { useMemo } from "react";
+import {
+  UrqlProvider,
+  ssrExchange,
+  cacheExchange,
+  fetchExchange,
+  createClient,
+} from "@urql/next";
+import { DashboardNavbar } from "@/components/dashboard/navbar";
 
 export default function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const [client, ssr] = useMemo(() => {
+    const ssr = ssrExchange();
+    const client = createClient({
+      url: process.env.NEXT_PUBLIC_GRAPHQL_URL ?? "",
+      exchanges: [cacheExchange, ssr, fetchExchange],
+      suspense: true,
+    });
+
+    return [client, ssr];
+  }, []);
+
   return (
     <section className="bg-gray-200 min-h-screen">
-      <DashboardNavbar />
-      <div className="container mx-auto">{children}</div>
+      <UrqlProvider client={client} ssr={ssr}>
+        <DashboardNavbar />
+        <div className="container mx-auto">{children}</div>
+      </UrqlProvider>
     </section>
   );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,35 @@
-export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+"use client";
+
+import { Suspense } from "react";
+import { gql, useQuery } from "@urql/next";
+
+export default function Page() {
+  return (
+    <Suspense>
+      <Profile />
+    </Suspense>
+  );
+}
+
+const GetUsersQuery = gql`
+  query {
+    users {
+      id
+      image
+      email
+    }
+  }
+`;
+
+function Profile() {
+  const [result] = useQuery({ query: GetUsersQuery });
+
+  return (
+    <section>
+      <h1>Profile Page</h1>
+      {result.data.users.map((user: any) => (
+        <p key={user.id}>{JSON.stringify(user, null, 2)}</p>
+      ))}
+    </section>
+  );
 }
